6881f79ddda9240fe63e940bac7b184edeb79d7a
Akshay Sharma <akshaysharma4664@gmail.com>
2024-08-02 21:55:19 +0530

    initial changes

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ import java.util.Date;
 public class Test{
     public static void main(String[] args) {
         System.out.println("This is java programme");
-        System.out.println("output for java programme");
+        System.out.println("hhsaoutput for java programme");
         System.out.println(new Date());
     }
 }
