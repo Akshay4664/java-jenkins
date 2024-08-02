30a6c6816e8a3041cfcafc74455a77540fb8e0ab
Akshay Sharma <akshaysharma4664@gmail.com>
2024-08-02 21:45:05 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+
+import java.util.Date;
+
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("This is java programme");
+        System.out.println("output for java programme");
+        System.out.println(new Date());
+    }
+}
